d58dd061a1c926260edaba2db362cd4f388d8b82
AJAIDEV S <146339839+ajwildr@users.noreply.github.com>
2023-10-31 11:18:18 +0530

    Update btreefisat.c

--- btreefisat.c
+++ btreefisat.c
@@ -6,35 +6,37 @@ struct node *lchild;
 int data;
 struct node *rchild;
 };
-struct node *root=NULL,*new,*r=NULL,*rt,*t1,*tp;
+struct node *root=NULL,*neww,*r=NULL,*rt,*t1,*tp;
 void create()
   {
     int v;
-    new=(struct node*)malloc(sizeof(struct node));
+    neww=(struct node*)malloc(sizeof(struct node));
     printf("Enter the value to the node: ");
     scanf("%d",&v);
-    new->data=v;
-    new->lchild=NULL;
-    new->rchild=NULL;
+    neww->data=v;
+    neww->lchild=NULL;
+    neww->rchild=NULL;
   }
 void search(struct node *rt)
 {
-  if(new->data < rt->data && lchild==NULL)
-     tr->lchild=new;
-  else if(new->data > rt->data && rt->rchild==NULL)
-     rt->rchild=new;
-  else if(new->data < rt->data && rt->lchild!=NULL)
-     search(rt->child);
-  else if(new->data==rt->data)
+  if(neww->data < rt->data && rt->lchild==NULL)
+     rt->lchild=neww;
+  else if(neww->data > rt->data && rt->rchild==NULL)
+     rt->rchild=neww;
+  else if(neww->data < rt->data && rt->lchild!=NULL)
+     search(rt->lchild);
+  else if(neww->data > rt->data && rt->rchild!=NULL)
+     search(rt->rchild);
+  else if(neww->data==rt->data)
      printf("\n same value has been entered twice!");
   else
-     print("\n Invalid Entry");
+     printf("\n Invalid Entry");
 }
 void insert()
   {
     create();
     if(root==NULL)
-       root=new;
+       root=neww;
     else
        search(root);
   }
@@ -44,9 +46,9 @@ void insert()
        r=rt;
      if(rt->lchild==NULL && rt->rchild==NULL)
        {
-         if(tr==r->rchild)
+         if(rt==r->lchild)
             r->lchild=NULL;
-         else if(rt==r->child)
+         else if(rt==r->rchild)
             r->rchild=NULL;
          else
              printf("\n Invalid Value");
@@ -57,16 +59,35 @@ void insert()
      else if(rt->lchild!=NULL && rt->rchild==NULL)
        {
          if(rt==r)
-           root=rt->child;
+           root=rt->lchild;
          else if(rt==r->lchild)
-           r->lchild=rt->child;
+           r->lchild=rt->lchild;
          else if(rt==r->rchild)
            r->rchild=rt->rchild;
          else
            printf("\n Invalid Value");
          free(rt);
        }
-    else if(rt->lchild!=NULL && rt->rchild!=NULL
+     else if(rt->lchild==NULL && rt->rchild!=NULL)
+     	{
+     	  if(rt==r)
+     	    {
+     	      root=rt->rchild;
+     	    }
+     	  else if(rt==r->lchild)
+     	    {
+     	      r->lchild=rt->rchild;
+     	    }
+     	  else if(rt==rt->rchild)
+     	    {
+     	      r->rchild=rt->rchild;
+     	    }
+     	  else
+     	     printf("\n Invalid Value");
+     	  free(rt);
+     	}
+     	
+    else if(rt->lchild!=NULL && rt->rchild!=NULL)
       {
        t1=rt->rchild;
        if(t1->rchild!=NULL)
@@ -86,8 +107,86 @@ void insert()
              
           
           }
+     else
+       {
+         rt->data=t1->data;
+         if(t1->lchild!=NULL)
+            rt->rchild=t1->lchild;
+         else
+            rt->rchild=NULL;
+         free(t1);   
+       }
           
       }  
+   else
+      printf("\n invalid Value");
          
        
     }
+ void dsearch(struct node *rt,int dval)
+{
+   if(rt->data==dval) 
+   {
+     deletenode(rt);
+   } 
+else if(dval < rt->data && rt->lchild!=NULL) 
+  {
+    r=rt;
+    dsearch(rt->lchild,dval );
+  } 
+ else if(dval > rt->data && rt->rchild!=NULL)
+  {
+    r=rt;
+    dsearch(rt->rchild,dval);
+  } 
+ else
+   printf("\nValue not found !");
+}
+
+void deletee()
+  {
+     int dval;
+     printf("enter the node to be deleted: ");
+     scanf("%d",&dval);
+     if(root==NULL)
+        printf("\n The BST is Empty");
+     else
+       dsearch(root,dval);
+  }
+void inorder(struct node *root)
+   {
+     if(root!=NULL)
+       {
+         
+         inorder(root->lchild);
+         printf("-%d-",root->data);
+         inorder(root->rchild);
+        }
+   }     
+ int main()
+{
+int ch;
+printf("choose a Binary Search Tree operation : \n1.Insertion \n 2.Deletion \n 3.Display \n4.Exit");
+while(1) {
+printf("\nEnter a choice :");
+scanf("%d",&ch);
+switch(ch) {
+case 1:
+insert();
+break;
+case 2:
+deletee();
+break;
+case 3:
+inorder(root);
+break;
+case 4:
+exit(0);
+break;
+default :
+printf("invalid value");
+break;
+}
+}
+return 0;
+}
